cac45de8d2fc933ae3c276720c81bb744e49d717
Hernan <hernan@mukiku.com>
2022-02-05 10:20:16 -0300

    'password_finish'

--- gen.py
+++ gen.py
@@ -1,5 +1,20 @@
+import random
+import string
+
+
+def generate_password(lenght):
+    password = []
+    for i in range(lenght):
+        password.append(random.choice(string.ascii_letters + string.digits +
+                        string.punctuation + string.hexdigits + string.octdigits))
+    return ''.join(password)
+
+
 def gen():
-    pass
+    length_pass = int(input("How long do you want your password to be? "))
+    password = generate_password(length_pass)
+    print("Generating password...")
+    print("Your password is: " + password)
 
 
 if __name__ == '__main__':
